a4091310a6ec131a47295f8ec684a9bef7d4fa94
David Amirkhanov <mail.websofter@gmail.com>
2024-09-08 07:06:34 +0300

    updt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { TestComponent } from './pages/test/test.component';
 import { NavigationComponent } from './shared/components/navigation/navigation.component';
+import { FooterComponent } from "./shared/components/footer/footer.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, TestComponent, NavigationComponent],
+  imports: [RouterOutlet, HomeComponent, TestComponent, NavigationComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
