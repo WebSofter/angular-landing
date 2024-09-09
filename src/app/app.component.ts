b833e923a6ca3dc0a12263029db999a9877163b2
David Amirkhanov <mail.websofter@gmail.com>
2024-09-10 02:29:09 +0300

    updt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,24 +18,34 @@ export class AppComponent implements OnInit {
   isMenuHidden = false;  // Флаг, указывающий на скрытие меню
   lastScrollTop = 0;     // Последняя позиция прокрутки
 
+/*
+   if (typeof document !== 'undefined') {
+  // Your browser-specific code here
+}
+*/
+
   ngOnInit() {
-    this.lastScrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    if (typeof document !== 'undefined') {
+      this.lastScrollTop = window?.pageYOffset || document?.documentElement.scrollTop;
+    }
   }
 
   // Отслеживание события прокрутки страницы
   @HostListener('window:scroll', [])
   onWindowScroll() {
-    let currentScrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    if (typeof document !== 'undefined') {
+      let currentScrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
-    if (currentScrollTop > this.lastScrollTop) {
-      // Прокрутка вниз - скрыть меню
-      this.isMenuHidden = true;
-    } else {
-      // Прокрутка вверх - показать меню
-      this.isMenuHidden = false;
+      if (currentScrollTop > this.lastScrollTop) {
+        // Прокрутка вниз - скрыть меню
+        this.isMenuHidden = true;
+      } else {
+        // Прокрутка вверх - показать меню
+        this.isMenuHidden = false;
+      }
+
+      // Обновляем последнюю позицию прокрутки
+      this.lastScrollTop = currentScrollTop <= 0 ? 0 : currentScrollTop;
     }
-
-    // Обновляем последнюю позицию прокрутки
-    this.lastScrollTop = currentScrollTop <= 0 ? 0 : currentScrollTop;
   }
 }
